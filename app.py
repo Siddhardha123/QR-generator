c6493f2772b54eedf6da2cad6e6df7a7b7985d83
siddhardha123 <siddhardha123123@gmail.com>
2022-06-23 00:32:30 +0530

    23-06-2022

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template,url_for
- 
+import pyqrcode
+from pyqrcode import QRCode
+import os
 app = Flask(__name__)
-
+picFolder = os.path.join("static","pics")
+app.config['uploadFolder'] = picFolder
 @app.route("/")
-
 def home():
-    return render_template("./index.html")
+    image = os.path.join(app.config['uploadFolder'],'myqr.png')
+    qr()
+    return render_template("./index.html",image1= image)
 
+def qr():
+    s = "hello vai"
+    url = pyqrcode.create(s)
+    url.png('myqr.png')
 if __name__ == '__main__':
     app.run(debug=True)
 
--- qr.py
+++
@@ -1,7 +0,0 @@
-
-import pyqrcode
-import png
-from pyqrcode import QRCode
-s = input("enter the link:-  ")
-url = pyqrcode.create(s)
-url.png('myqr.png', scale=6)
