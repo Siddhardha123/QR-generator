b2ee162e48103167fc32c25743a95c2c350df5d9
Siddhardha123 <71877477+Siddhardha123@users.noreply.github.com>
2022-06-24 07:48:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
+#imports
 from flask import Flask, render_template,url_for
 import pyqrcode
 from pyqrcode import QRCode
 import os
+#creation of app
 app = Flask(__name__)
+#folder which contains images
 picFolder = os.path.join("static","pics")
 app.config['uploadFolder'] = picFolder
+#home page route
 @app.route("/")
 def home():
     image = os.path.join(app.config['uploadFolder'],'myqr.png')
     qr()
     return render_template("./index.html",image1= image)
-
+#function to generate QR
 def qr():
     s = "hello vai"
     url = pyqrcode.create(s)
