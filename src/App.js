16d8598a12fd31192a9917d350e813195ab1e26a
siddhardha varma <sid@pop-os.localdomain>
2022-12-19 17:18:01 +0530

    re-init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Qrframe from './components/Qrframe';
+function App() {
+  return (
+    <>
+       <Qrframe />
+    </>
+  );
+}
+
+export default App;
